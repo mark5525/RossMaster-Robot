34c537618e41364f957730b3392889509885852e
dschagen <dschagen6@gmail.com>
2026-01-29 17:58:54 -0500

    Added a while loop to the set motor command to keep the script active

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ import time
 
 if __name__ == '__main__':
     ros = Rosmaster()
-
-    ros.set_motor(10, 10, 10, 10)
+    try:
+        while True:
+            ros.set_motor(10, 10, 10, 10)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Shutting down")
