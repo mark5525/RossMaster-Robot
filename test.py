05bbfa446fbb76d3ca91373be0ebe9562e1e8525
dschagen <dschagen6@gmail.com>
2026-02-11 19:39:49 -0500

    Trying out a forward and reverse command

--- test.py
+++ test.py
@@ -3,15 +3,28 @@ import time
 
 if __name__ == '__main__':
     ros = Rosmaster()
+
+    forward_speed = 30
+    run_time = 3   # seconds
+
     try:
-        while True:
-            ros.set_motor(30, 30, 30, 30)
-            time.sleep(1)
+        # ---- Move Forward ----
+        print("Moving forward...")
+        ros.set_motor(forward_speed, forward_speed, forward_speed, forward_speed)
+        time.sleep(run_time)
+
+        # ---- Stop briefly (optional but recommended) ----
+        ros.set_motor(0, 0, 0, 0)
+        time.sleep(0.5)
+
+        # ---- Move Backward ----
+        print("Reversing...")
+        ros.set_motor(-forward_speed, -forward_speed, -forward_speed, -forward_speed)
+        time.sleep(run_time)
+
     except KeyboardInterrupt:
         print("Shutting down")
+
     finally:
         # Always stop motors on exit
-        try:
-            ros.set_motor(0, 0, 0, 0)
-        except Exception:
-            pass
+        ros.set_motor(0, 0, 0, 0)
